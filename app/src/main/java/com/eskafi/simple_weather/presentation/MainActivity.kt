1faa20c9937bb3264b5680e0c89d62b53f9c31ba
mahdiyeh <mah.eskafi1990@gmail.com>
2023-01-09 14:53:04 +0100

    create views  -add xml files  -define dimens and styles

---
+++ app/src/main/java/com/eskafi/simple_weather/presentation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.eskafi.simple_weather.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eskafi.simple_weather.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
